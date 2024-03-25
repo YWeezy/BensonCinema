3b814c3f0b48a93026e7d2100c3d3ba7ad22aa19
Quinten van den Beukel <quinten1quinten@hotmail.com>
2024-03-25 23:22:08 +0100

    performance model, access and logic layer. minor changes to the json file

---
+++ DataModels/PerformanceModel.cs
@@ -0,0 +1,29 @@
+using System.Data.Common;
+using System.Text.Json.Serialization;
+
+class PerformanceModel {
+
+    [JsonPropertyName("id")]
+    public int id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string name { get; set; }
+    
+    [JsonPropertyName("startDate")]
+    public DateTime startDate { get; set; }
+
+    [JsonPropertyName("endDate")]
+    public DateTime endDate { get; set; }
+
+    [JsonPropertyName("hallId")]
+    public int hallId { get; set; }
+
+    public PerformanceModel(int id, string name, DateTime startDate, DateTime endDate, int hallId) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.hallId = hallId;
+    }
+
+}
--- DataModels/Ticket.cs
+++ DataModels/Ticket.cs
@@ -1,17 +1,17 @@
 public class Ticket
 {
-    public User name { set; get; }
-    public static int ticketID { set; get; }
-    public double price { set; get; }
-    public int seat { set; get; }
-    public int location { set; get; }
+    // public User name { set; get; }
+    // public static int ticketID { set; get; }
+    // public double price { set; get; }
+    // public int seat { set; get; }
+    // public int location { set; get; }
 
-    public Ticket(User user, double price, int seat, int location)
-    {
-        this.name = user;
-        this.price = price;
-        this.seat = seat;
-        this.location = location;
-        ticketID++;
-    }
+    // public Ticket(User user, double price, int seat, int location)
+    // {
+    //     this.name = user;
+    //     this.price = price;
+    //     this.seat = seat;
+    //     this.location = location;
+    //     ticketID++;
+    // }
 }
