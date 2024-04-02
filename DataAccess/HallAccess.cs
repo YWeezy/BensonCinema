1dde66c26ab9045f4fce4188d0e50e6976d605dd
Wesley <wesleyandrade0305@gmail.com>
2024-04-02 13:34:59 +0200

    Hall access fix
    changed path

--- DataAccess/HallAccess.cs
+++ DataAccess/HallAccess.cs
@@ -1,7 +1,7 @@
 using System.Text.Json;
 static class LocationAccess
 {
-    static private readonly string path = "./DataSources/locations.json";
+    static private readonly string path = "./DataSources/halls.json";
     public static List<LocationModel> Locationget(){
         List<LocationModel> locs = new List <LocationModel>();
         try
