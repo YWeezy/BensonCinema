8710e0b68a692fa95537871a4f58d834012269cf
Ach <achraf.aarab@outlook.com>
2024-04-24 13:51:33 +0200

    working changes

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -4,14 +4,30 @@ namespace test;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestEncryption(){
-        AccountModel user =  new ("achraf.aarab@benson.com", "Achraf Aarab", "achraf", UserRole.User);
+    public void TestEncryption()
+    {
+        AccountModel user = new("achraf.aarab@benson.com", "Achraf Aarab", "achraf", UserRole.User);
         Assert.AreNotEqual(user.Password, "achraf");
     }
 
     [TestMethod]
-    public void TestEncryption(){
-        AccountModel user =  new ("achraf.aarab@benson.com", "Achraf Aarab", "achraf", UserRole.User);
-        Assert.AreNotEqual(user.Password, "achraf");
+    public void TestAccount()
+    {
+        AccountModel user = new("achraf.aarab@benson.com", "Achraf Aarab", "achraf", UserRole.User);
+        Assert.AreEqual(user.EmailAddress, "achraf.aarab@benson.com");
+        Assert.AreEqual(user.FullName, "Achraf Aarab");
+        Assert.AreEqual(user.Role, UserRole.User);
+    }
+
+    [TestMethod]
+    public void TestUniqueAccount()
+    {
+        AccountsLogic accountsLogic = new AccountsLogic();
+        AccountModel user = new("achraf.aarab@benson.com", "Achraf Aarab", "achraf", UserRole.User);
+        accountsLogic.UpdateList(user);
+        bool isUnique = accountsLogic.CheckIfInDB(user);
+        Console.WriteLine(isUnique);
+        Assert.AreEqual(isUnique, false);
+
     }
 }
