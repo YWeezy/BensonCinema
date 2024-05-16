e7441cbc4e9be401451494cbdc9775c0ed9b8074
GREED <pratikadhikari10@gmail.com>
2024-05-16 20:47:30 +0200

    error

--- test/TestPerformance.cs
+++ test/TestPerformance.cs
@@ -1,101 +1,105 @@
-///using System.Reflection;
-///namespace test;
-///
-///[TestClass]
-///public class TestPerformance
-///{
-///
-///    public static string path;
-///
-///    [AssemblyInitialize]
-///    public static void Initialize(TestContext context)
-///    {
-///        path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "../../../TestDataSources/performances.json");
-///    }
-///
-///    [TestMethod]
-///    public void TestGetPerformances()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        
-///        // Act
-///        List<PerformanceModel> performances = performanceLogic.GetPerformances();
-///
-///        // Assert
-///        Assert.AreEqual(0, performances.Count);
-///    }
-///
-///    [TestMethod]
-///    public void TestGetNewId()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        
-///        // Act
-///        int newId = performanceLogic.GetNewId();
-///
-///        // Assert
-///        Assert.AreEqual(1, newId);
-///    }
-///
-///    [TestMethod]
-///    public void TestUpdateList()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        int newId = performanceLogic.GetNewId();
-///
-///        PerformanceModel performanceToAdd = new PerformanceModel(newId, "Demo A", DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), 1, true);
-///        
-///        // Act
-///        performanceLogic.UpdateList(performanceToAdd);
-///
-///        // Assert
-///        Assert.IsTrue(performanceLogic.GetPerformances().Contains(performanceToAdd));
-///    }
-///
-///    [TestMethod]
-///    public void TestGetTotalPerformances()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        
-///        // Act
-///        int totalPerformances = performanceLogic.GetTotalPerformances();
-///
-///        // Assert
-///        Assert.AreEqual(1, totalPerformances);
-///    }
-///
-///    [TestMethod]
-///    public void TestGetPerfById()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        int id = 1;
-///        
-///        // Act
-///        PerformanceModel performance = performanceLogic.GetPerfById(id);
-///
-///        // Assert
-///        Assert.AreEqual(id, performance.id);
-///    }
-///
-///    [TestMethod]
-///    public void TestDeletePerformance()
-///    {
-///        // Arrange
-///        PerformanceLogic performanceLogic = new PerformanceLogic(path);
-///        int idToDelete = 1; // ID of the performance to delete
-///        
-///        // Act
-///        bool isDeleted = performanceLogic.DeletePerformance(idToDelete);
-///
-///        // Assert
-///        Assert.IsTrue(isDeleted);
-///        Assert.IsNull(performanceLogic.GetPerfById(idToDelete));
-///    }
-///
-///
-///}
+using System.Reflection;
+namespace test;
+
+[TestClass]
+public class TestPerformance
+{
+
+    public static string performancePath;
+    public static string hallPath;
+
+    [AssemblyInitialize]
+    public static void Initialize(TestContext context)
+    {
+        performancePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "../../../TestDataSources/performances.json");
+        hallPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "../../../TestDataSources/halls.json");
+    }
+
+    [TestMethod]
+    public void TestGetPerformances()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        
+        // Act
+        List<PerformanceModel> performances = performanceLogic.GetPerformances();
+
+        // Assert
+        Assert.AreEqual(0, performances.Count);
+    }
+
+    [TestMethod]
+    public void TestGetNewId()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        
+        // Act
+        int newId = performanceLogic.GetNewId();
+
+        // Assert
+        Assert.AreEqual(1, newId);
+    }
+
+    [TestMethod]
+    public void TestUpdateList()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        int newId = performanceLogic.GetNewId();
+
+        List<Dictionary<string, object>> listOfDicts = new();
+
+        PerformanceModel performanceToAdd = new PerformanceModel(newId, "Demo A", "Test description", DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), 1, listOfDicts, true);
+        
+        // Act
+        performanceLogic.UpdateList(performanceToAdd);
+
+        // Assert
+        Assert.IsTrue(performanceLogic.GetPerformances().Contains(performanceToAdd));
+    }
+
+    [TestMethod]
+    public void TestGetTotalPerformances()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        
+        // Act
+        int totalPerformances = performanceLogic.GetTotalPerformances();
+
+        // Assert
+        Assert.AreEqual(1, totalPerformances);
+    }
+
+    [TestMethod]
+    public void TestGetPerfById()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        int id = 1;
+        
+        // Act
+        PerformanceModel performance = performanceLogic.GetPerfById(id);
+
+        // Assert
+        Assert.AreEqual(id, performance.id);
+    }
+
+    [TestMethod]
+    public void TestDeletePerformance()
+    {
+        // Arrange
+        PerformanceLogic performanceLogic = new PerformanceLogic(performancePath);
+        int idToDelete = 1; // ID of the performance to delete
+        
+        // Act
+        bool isDeleted = performanceLogic.DeletePerformance(idToDelete);
+
+        // Assert
+        Assert.IsTrue(isDeleted);
+        Assert.IsNull(performanceLogic.GetPerfById(idToDelete));
+    }
+
+
+}
--- test/TestSchedules.cs
+++ test/TestSchedules.cs
@@ -1,33 +1,45 @@
-///namespace test
-///{ 
-///
-///[TestClass]
-///    public class TestEmployeeSchedule
-///    {
-///        public static string path;
-///
-///        [AssemblyInitialize]
-///        public static void Initialize(TestContext context)
-///        {
-///            string excecutingAssemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
-///            path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(excecutingAssemblyLocation), "../../../TestDataSources/schedule.json" );
-///
-///        }
-///
-///        [TestMethod]
-///        public void TestIsValidDate_ValidDate_ReturnsTrue()
-///        {
-///            string validDate = "01-01-2024";
-///            bool result = EmployeeSchedule.IsValidDate(validDate);
-///            Assert.IsTrue(result);
-///        }
-///
-///        [TestMethod]
-///        public void TestIsValidDate_InvalidDate_ReturnsFalse()
-///        {
-///            string invalidDate = "32-01-2024";
-///            bool result = EmployeeSchedule.IsValidDate(invalidDate);
-///            Assert.IsFalse(result);
-///        }
-///    }
-///}
+using System.Reflection;
+
+namespace test
+{ 
+
+[TestClass]
+    public class TestEmployeeSchedule
+    {
+        public static string schedulePath;
+
+        //[TestMethod]
+        //public void TestIsValidDate_ValidDate_ReturnsTrue()
+        //{
+        //    string validDate = "01-01-2024";
+        //    bool result = EmployeeSchedule.IsValidDate(validDate);
+        //    Assert.IsTrue(result);
+        //}
+    	//
+        //[TestMethod]
+        //public void TestIsValidDate_InvalidDate_ReturnsFalse()
+        //{
+        //    string invalidDate = "32-01-2024";
+        //    bool result = EmployeeSchedule.IsValidDate(invalidDate);
+        //    Assert.IsFalse(result);
+        //}
+
+        //[TestMethod]
+        //public void TestGetUpdateSchedules()
+        //{
+        //// Arrange
+        //ScheduleLogic scheduleLogic = new ScheduleLogic(schedulePath);
+        //int newId = scheduleLogic.getID();
+//
+        //List<Dictionary<string, object>> listOfDicts = new();
+//
+        //ScheduleModel updateToAdd = new ScheduleModel(newId, "John", "01-01-2024", "8", "08:00", "16:00", new PerformanceModel(1, "Demo A", "Test description", DateTime.Now.AddDays(1), DateTime.Now.AddDays(2), 1, listOfDicts, true), true);
+        //
+        //// Act
+        //scheduleLogic.UpdateList(updateToAdd);
+//
+        //// Assert
+        //Assert.IsTrue(scheduleLogic.GetSchedules().Contains(updateToAdd));
+        //}
+    }
+}
