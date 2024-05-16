cf7c1b269c229f8f7f778f86e2880b73b8d6bde0
GREED <pratikadhikari10@gmail.com>
2024-05-16 17:25:55 +0200

    Update TestSchedules.cs

--- test/TestSchedules.cs
+++ test/TestSchedules.cs
@@ -1,33 +1,33 @@
-namespace test
-{ 
-
-[TestClass]
-    public class TestEmployeeSchedule
-    {
-        public static string path;
-
-        [AssemblyInitialize]
-        public static void Initialize(TestContext context)
-        {
-            string excecutingAssemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
-            path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(excecutingAssemblyLocation), "../../../TestDataSources/schedule.json" );
-
-        }
-
-        [TestMethod]
-        public void TestIsValidDate_ValidDate_ReturnsTrue()
-        {
-            string validDate = "01-01-2024";
-            bool result = EmployeeSchedule.IsValidDate(validDate);
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public void TestIsValidDate_InvalidDate_ReturnsFalse()
-        {
-            string invalidDate = "32-01-2024";
-            bool result = EmployeeSchedule.IsValidDate(invalidDate);
-            Assert.IsFalse(result);
-        }
-    }
-}
+///namespace test
+///{ 
+///
+///[TestClass]
+///    public class TestEmployeeSchedule
+///    {
+///        public static string path;
+///
+///        [AssemblyInitialize]
+///        public static void Initialize(TestContext context)
+///        {
+///            string excecutingAssemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+///            path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(excecutingAssemblyLocation), "../../../TestDataSources/schedule.json" );
+///
+///        }
+///
+///        [TestMethod]
+///        public void TestIsValidDate_ValidDate_ReturnsTrue()
+///        {
+///            string validDate = "01-01-2024";
+///            bool result = EmployeeSchedule.IsValidDate(validDate);
+///            Assert.IsTrue(result);
+///        }
+///
+///        [TestMethod]
+///        public void TestIsValidDate_InvalidDate_ReturnsFalse()
+///        {
+///            string invalidDate = "32-01-2024";
+///            bool result = EmployeeSchedule.IsValidDate(invalidDate);
+///            Assert.IsFalse(result);
+///        }
+///    }
+///}
