1c0278c11896f6ce1c5a66287db7b577fad342b6
GREED <pratikadhikari10@gmail.com>
2024-05-16 20:53:14 +0200

    Update TestSchedules.cs

--- test/TestSchedules.cs
+++ test/TestSchedules.cs
@@ -8,21 +8,21 @@ namespace test
     {
         public static string schedulePath;
 
-        //[TestMethod]
-        //public void TestIsValidDate_ValidDate_ReturnsTrue()
-        //{
-        //    string validDate = "01-01-2024";
-        //    bool result = EmployeeSchedule.IsValidDate(validDate);
-        //    Assert.IsTrue(result);
-        //}
-    	//
-        //[TestMethod]
-        //public void TestIsValidDate_InvalidDate_ReturnsFalse()
-        //{
-        //    string invalidDate = "32-01-2024";
-        //    bool result = EmployeeSchedule.IsValidDate(invalidDate);
-        //    Assert.IsFalse(result);
-        //}
+        //deze niet kwinten. keep it commented//[TestMethod]
+        //deze niet kwinten. keep it commented//public void TestIsValidDate_ValidDate_ReturnsTrue()
+        //deze niet kwinten. keep it commented//{
+        //deze niet kwinten. keep it commented//    string validDate = "01-01-2024";
+        //deze niet kwinten. keep it commented//    bool result = EmployeeSchedule.IsValidDate(validDate);
+        //deze niet kwinten. keep it commented//    Assert.IsTrue(result);
+        //deze niet kwinten. keep it commented//}
+    	//deze niet kwinten. keep it commented//
+        //deze niet kwinten. keep it commented//[TestMethod]
+        //deze niet kwinten. keep it commented//public void TestIsValidDate_InvalidDate_ReturnsFalse()
+        //deze niet kwinten. keep it commented//{
+        //deze niet kwinten. keep it commented//    string invalidDate = "32-01-2024";
+        //deze niet kwinten. keep it commented//    bool result = EmployeeSchedule.IsValidDate(invalidDate);
+        //deze niet kwinten. keep it commented//    Assert.IsFalse(result);
+        //deze niet kwinten. keep it commented//}
 
         //[TestMethod]
         //public void TestGetUpdateSchedules()
