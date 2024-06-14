0c98918cc2258b05f053139c3d505f32de0a16df
Wesley <wesleyandrade0305@gmail.com>
2024-06-14 14:00:40 +0200

    test schedules

--- benson/Logic/ScheduleLogic.cs
+++ benson/Logic/ScheduleLogic.cs
@@ -43,9 +43,16 @@ public class ScheduleLogic
         return _schedules.Where(s => s.Worker == employeeName).ToList();
     }
 
+    
+
     public List<SchedulesModel> GetSchedules()
     {
         return _schedules.ToList();
+    }
+
+    public List<SchedulesModel> GetActiveSchedules()
+    {
+        return _schedules.Where(s => s.Active == true).ToList();
     }
 
     public bool RemoveSchedule(string scheduleID)
