27f8271ed1da9d8ac427c9a74ca9715c5fb13acb
GitButler <gitbutler@gitbutler.com>
2024-08-09 23:15:50 +0200

    added log hello added log bye

--- benson/Presentation/Program.cs
+++ benson/Presentation/Program.cs
@@ -5,6 +5,9 @@ public class Program
     public static void Main()
     {
         Menu.Start();
+        Console.WriteLine("hello");
+        Console.WriteLine("Bye");
+
         // ViewReviews rev = new ViewReviews(1);
         // rev.AddReview();
     }
